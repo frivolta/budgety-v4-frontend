27b67c5d2507162b4f8549f08ce1267f75ce2dbb
frivolta <rivoltafilippo@gmail.com>
2020-01-21 23:00:38 +0100

    :white_check_mark: Adding unit tests

--- src/components/Card/AltCard.tsx
+++ src/components/Card/AltCard.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 interface ICard {
   className?: string;
+  children: ReactNode;
 }
 
 export const AltCard: React.FC<ICard> = props =>
-  <div className={`AltCard ${props.className}`}>
+  <div className={`AltCard ${props.className}`} data-testid="AltCard">
     <div className="AltCard__content">
       {props.children}
     </div>
