c9b21b89719d6f6ac3fbcd49ab92c7bf141a4b00
frivolta <rivoltafilippo@gmail.com>
2020-01-24 15:28:25 +0100

    :lipstick: Styling toaster

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import { SignupPage } from './pages/signup';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-toast.configure();
+toast.configure({
+  className: 'Toaster',
+  draggable: true,
+  draggablePercent: 60,
+  autoClose: 20000
+});
 
 const App: React.FC = () => {
   return <SignupPage />;
