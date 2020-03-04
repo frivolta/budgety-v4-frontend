bb0da0d760ddee842afb5ea257a2d78f5e503581
frivolta <rivoltafilippo@gmail.com>
2020-03-04 21:52:43 +0100

    :features: Sidebar closing after clicking button

--- src/components/Sidenav/Sidenav.tsx
+++ src/components/Sidenav/Sidenav.tsx
@@ -6,11 +6,16 @@ import { Button } from '../Button/Button';
 import { useSidenavValue } from '../../context/useSidenavValue';
 import { FiltersContainer } from '../../container/FiltersContainer/FiltersContainer';
 
-//import { MenuItemList } from '../Menu/MenuItemList'
-
 export const Sidenav = () => {
   const { sidenavIsOpen, setSidenavIsOpen } = useSidenavValue();
   let history = useHistory();
+
+  const _handleButtonClickAction = (redirectPath: string): void => {
+    // Redirect to supplied path
+    history.push(redirectPath);
+    // Trigger sidebar close
+    setSidenavIsOpen(false);
+  };
 
   return (
     <aside className={sidenavIsOpen ? `Sidenav Sidenav--isActive` : `Sidenav`}>
@@ -22,9 +27,8 @@ export const Sidenav = () => {
         >
           <img src="/images/close-menu.svg" alt="close menu" />
         </div>
-        {/*<MenuItemList />*/}
-        <Button text="New Expense" handleClick={() => history.push('/expense/add')} />
-        <Button text="All Expenses" handleClick={() => history.push('/')} />
+        <Button text="New Expense" handleClick={() => _handleButtonClickAction('/expense/add')} />
+        <Button text="All Expenses" handleClick={() => _handleButtonClickAction('/')} />
         <FiltersContainer />
       </div>
     </aside>
